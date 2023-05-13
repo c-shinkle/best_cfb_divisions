5eb3d5e00ed6c1f3471f6e0befa588514576cfc7
Christian Shinkle <cshinkle2018@gmail.com>
2023-05-13 14:29:31 -0500

    went back to not cloning

--- src/division_pairs.rs
+++ src/division_pairs.rs
@@ -4,21 +4,19 @@ use std::collections::HashMap;
 
 pub fn get_all_division_pairs(conference: &Conference) -> Vec<DivisionPair> {
     let len = conference.len();
-    let index_combinations = (0..len).combinations(len / 2).collect::<Vec<Vec<usize>>>();
+    let index_combinations: Vec<Vec<usize>> = (0..len).combinations(len / 2).collect();
 
     let mut map = HashMap::with_capacity(index_combinations.len() / 2);
     let index_into = |i: usize| conference[i];
     for combo in index_combinations {
         if !map.contains_key(&combo) {
-            let complement: Vec<usize> = (0..len).filter(|i| !combo.contains(i)).collect();
-            if !map.contains_key(&complement) {
-                map.insert(
-                    combo.clone(),
-                    (
-                        combo.into_iter().map(index_into).collect(),
-                        complement.into_iter().map(index_into).collect(),
-                    ),
+            let comp: Vec<usize> = (0..len).filter(|i| !combo.contains(i)).collect();
+            if !map.contains_key(&comp) {
+                let division_pair = (
+                    combo.iter().copied().map(index_into).collect(),
+                    comp.into_iter().map(index_into).collect(),
                 );
+                map.insert(combo, division_pair);
             }
         }
     }
