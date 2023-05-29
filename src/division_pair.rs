9f5a2b694b1bd20025e96e8d9bb8bbbe0784ebea
Christian Shinkle <cshinkle2018@gmail.com>
2023-05-28 23:35:09 -0500

    HashSet algorithm is stable

--- src/division_pair.rs
+++ src/division_pair.rs
@@ -2,32 +2,60 @@ use crate::types::DivisionPair;
 use crate::Conference;
 use itertools::Itertools;
 use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
+#[derive(Debug)]
+struct CombinationIndexes {
+    indexes: Vec<usize>,
+}
+
+impl PartialEq for CombinationIndexes {
+    fn eq(&self, other: &CombinationIndexes) -> bool {
+        self.indexes == other.indexes
+            || get_compliment(self.indexes.len() * 2, &self.indexes) == other.indexes
+    }
+}
+
+impl Eq for CombinationIndexes {}
+
+impl Hash for CombinationIndexes {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let comp = get_compliment(self.indexes.len() * 2, &self.indexes);
+        if self.indexes[0] == 0 {
+            self.indexes.hash(state);
+            comp.hash(state);
+        } else {
+            comp.hash(state);
+            self.indexes.hash(state);
+        }
+    }
+}
+
+fn get_compliment(len: usize, index_combo: &[usize]) -> Vec<usize> {
+    (0..len).filter(|i| !index_combo.contains(i)).collect()
+}
 
 pub fn get_all_division_pairs(conference: &Conference) -> Vec<DivisionPair> {
     let len = conference.len();
-    let index_combinations: Vec<Vec<usize>> = (0..len).combinations(len / 2).collect();
-    let division_pairs_len = index_combinations.len() / 2;
+    let indexes_combinations: Vec<Vec<usize>> = (0..len).combinations(len / 2).collect();
 
-    let mut set: HashSet<Vec<usize>> = HashSet::with_capacity(division_pairs_len);
-    let index_into = |i: usize| conference[i];
+    let mut set: HashSet<CombinationIndexes> =
+        HashSet::with_capacity(indexes_combinations.len() / 2);
 
-    for index_combo in index_combinations {
-        if !set.contains(&index_combo) {
-            let comp = (0..len).filter(|i| !index_combo.contains(i)).collect_vec();
-            if !set.contains(&comp) {
-                set.insert(index_combo);
-            }
-        }
+    for indexes in indexes_combinations {
+        set.insert(CombinationIndexes { indexes });
     }
 
+    let index_into = |i: usize| conference[i];
     set.into_iter()
-        .map(|index_combo| {
+        .map(|combo| {
+            let indexes = combo.indexes;
             (
-                index_combo.iter().copied().map(index_into).collect(),
                 (0..len)
-                    .filter(|i| !index_combo.contains(i))
+                    .filter(|i| !indexes.contains(i))
                     .map(index_into)
                     .collect(),
+                indexes.into_iter().map(index_into).collect(),
             )
         })
         .collect()
@@ -47,13 +75,12 @@ mod test {
         let actual: Vec<DivisionPair> = get_all_division_pairs(&conference);
 
         let expected: HashSet<DivisionPair> = HashSet::from([
-            (vec!["A", "B"], vec!["C", "D"]),
-            (vec!["A", "C"], vec!["B", "D"]),
-            (vec!["A", "D"], vec!["B", "C"]),
+            (vec!["C", "D"], vec!["A", "B"]),
+            (vec!["B", "D"], vec!["A", "C"]),
+            (vec!["B", "C"], vec!["A", "D"]),
         ]);
 
         assert_eq!(actual.len(), expected.len());
-        println!("{:?}", actual);
         assert!(actual.iter().all(|x| expected.contains(x)));
     }
 }
