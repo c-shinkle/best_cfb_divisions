42d8bbd2ad30cc9171139176072c064d384a8416
Christian Shinkle <cshinkle2018@gmail.com>
2023-07-02 19:17:04 -0500

    updated script

--- src/script.rs
+++ src/script.rs
@@ -4,6 +4,7 @@
 // use std::fs::File;
 // use std::io::Write;
 
+// #[allow(dead_code)]
 // pub fn write_all_stadium_distance_pairs_to_file(stadium_names: &[&str]) {
 //     let client = Client::new();
 //     let mut file = File::create("output.txt").expect("creating file failed!");
@@ -11,8 +12,7 @@
 //     let combinations = stadium_names
 //         .iter()
 //         .tuple_combinations::<(&&str, &&str)>()
-//         .map(|(a, b)| (*a, *b))
-//         .collect::<Vec<(&str, &str)>>();
+//         .collect::<Vec<(&&str, &&str)>>();
 //     for (origin, destination) in combinations {
 //         let params = [
 //             ("outputFormat", "json"),
@@ -27,22 +27,29 @@
 //             .expect("The web client failed!");
 
 //         if !request.status().is_success() {
-//             println!("Received status code {}", request.status().as_str());
-//             return;
+//             panic!("Received status code {}", request.status().as_str());
 //         }
 
 //         let json = request.text().expect("The json failed!");
 //         // println!("Response Body: {}", json);
 
 //         let value = serde_json::from_str::<Value>(&json).expect("serde_json failed!");
-//         let routes = value["routes"].as_array().expect("No 'routes' found!");
-//         let route = routes[0].as_object().expect("No 'route' found!");
+//         let routes = value
+//             .get("routes")
+//             .and_then(|routes| routes.as_array())
+//             .expect("No 'routes' found!");
+//         let route = routes
+//             .get(0)
+//             .and_then(|route| route.as_object())
+//             .expect("No 'route' found!");
 //         let legs = route
 //             .get("legs")
-//             .unwrap()
-//             .as_array()
+//             .and_then(|legs| legs.as_array())
 //             .expect("No 'legs' found!");
-//         let leg = legs[0].as_object().expect("No 'leg' found!");
+//         let leg = legs
+//             .get(0)
+//             .and_then(|leg| leg.as_object())
+//             .expect("No 'leg' found!");
 //         let distance = leg.get("distance").expect("No 'distance' found!");
 //         let text = distance.get("text").expect("No 'text' found!");
 //         let miles_string = text.to_string();
@@ -50,16 +57,13 @@
 //             .trim_matches('"')
 //             .split(' ')
 //             .next()
-//             .unwrap_or_else(|| panic!("Couldn't parse: {}", miles_string))
+//             .expect(&format!("Couldn't parse: {}", miles_string))
 //             .replace(',', "");
-//         println!(
-//             "The distance from {} to {} is {} miles!",
-//             origin, destination, raw_miles
-//         );
+//         println!("The distance from {origin} to {destination} is {raw_miles} miles!");
 
 //         let miles = raw_miles
 //             .parse::<f64>()
-//             .unwrap_or_else(|error| panic!("{}", error));
+//             .unwrap_or_else(|error| panic!("{error}"));
 //         let temp = format!("(\"{}\", \"{}\") => {:.0},\n", origin, destination, miles);
 //         file.write_all(temp.as_bytes())
 //             .expect("file writing failed!");
