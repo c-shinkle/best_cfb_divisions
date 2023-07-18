5b75fee9383bf6dafe2e15862c28af180c89636a
Christian Shinkle <cshinkle2018@gmail.com>
2023-07-17 22:38:28 -0500

    discovered method to reduce unnecessary work

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use best_cfb_divisions::pod::algorithm::find_closest_pods;
+#[allow(unused_imports)]
 use best_cfb_divisions::pod::combo::get_all_pod_combinations;
 #[allow(unused_imports)]
-use best_cfb_divisions::{
-    division::algorithm::find_closest_divisions, pod::algorithm::find_closest_pods,
-};
+use std::fs::File;
+#[allow(unused_imports)]
+use std::io::Write;
 
 fn main() {
     let conference = [
@@ -23,11 +25,11 @@ fn main() {
         // "USC",
         // "Wisconsin",
     ];
-    // find_closest_pods::<4>(&conference);
-    let all_pod_combos = get_all_pod_combinations::<3>(&conference);
-    for combo in all_pod_combos {
-        for (i, pod) in combo.into_iter().enumerate() {
-            println!("Pod {}: {:?}", i + 1, pod);
-        }
-    }
+    find_closest_pods::<3>(&conference);
+    // let mut file = File::create("./all_three_pod_combos.txt").unwrap();
+    // let all_pod_combos = get_all_pod_combinations::<3>(&conference);
+    // for combo in all_pod_combos {
+    //     let strings: Vec<String> = combo.into_iter().map(|pod| format!("{pod:?}")).collect();
+    //     writeln!(file, "[{}]", strings.join(", ")).unwrap();
+    // }
 }
--- src/pod/combo.rs
+++ src/pod/combo.rs
@@ -1,11 +1,22 @@
-use std::sync::Arc;
-
 use crate::types::*;
 use itertools::Itertools;
 
 const POD_RATIO: usize = 4;
+const PROTECTED_PAIRS: [TeamPair; 6] = [
+    ("Indiana", "Purdue"),
+    ("Michigan State", "Michigan"),
+    ("Northwestern", "Illinois"),
+    ("Ohio State", "Michigan"),
+    ("Purdue", "Illinois"),
+    ("Rutgers", "Maryland"),
+    // ("UCLA", "USC"),
+    // ("Iowa", "Minnesota"),
+    // ("Iowa", "Nebraska"),
+    // ("Iowa", "Wisconsin"),
+    // ("Minnesota", "Wisconsin"),
+];
 pub type Pod = Vec<Team>;
-pub type PodCombo<const N: usize> = [Arc<Pod>; N];
+pub type PodCombo<const N: usize> = [Pod; N];
 
 pub fn get_all_pod_combinations<const POD_COUNT: usize>(
     conference: &Conference,
@@ -14,30 +25,17 @@ pub fn get_all_pod_combinations<const POD_COUNT: usize>(
         conference.len() / POD_COUNT == POD_RATIO,
         "The ration between Conference length and Pod count must equal 4!"
     );
-    // let protected_pairs: Vec<TeamPair> = vec![
-    //     ("Indiana", "Purdue"),
-    //     ("Michigan State", "Michigan"),
-    //     ("Northwestern ", "Illinois"),
-    //     ("Ohio State ", "Michigan"),
-    //     ("Purdue", "Illinois"),
-    //     ("Rutgers", "Maryland"),
-    //     ("UCLA", "USC"),
-    //     ("Iowa", "Minnesota"),
-    //     ("Iowa", "Nebraska"),
-    //     ("Iowa", "Wisconsin"),
-    //     ("Minnesota", "Wisconsin"),
-    // ];
 
     helper::<POD_COUNT>(Vec::new(), conference.to_owned(), conference)
 }
 
 fn helper<const POD_COUNT: usize>(
-    mut pods_so_far: Vec<Arc<Pod>>,
+    mut pods_so_far: Vec<Pod>,
     remaining_teams: Vec<Team>,
     conference: &Conference,
 ) -> Vec<PodCombo<POD_COUNT>> {
     if remaining_teams.len() <= POD_RATIO {
-        pods_so_far.push(Arc::new(remaining_teams));
+        pods_so_far.push(remaining_teams);
         let pod_combo: PodCombo<POD_COUNT> = pods_so_far
             .try_into()
             .expect("Pod count didn't match last pod");
@@ -52,6 +50,9 @@ fn helper<const POD_COUNT: usize>(
     let mut all_pod_combos: Vec<PodCombo<POD_COUNT>> = Vec::new();
 
     for remaining_pod in remaining_pods {
+        if !is_pod_valid(&remaining_pod) {
+            continue;
+        }
         let new_remaining_teams: Vec<Team> = remaining_teams
             .iter()
             .copied()
@@ -59,7 +60,7 @@ fn helper<const POD_COUNT: usize>(
             .collect();
 
         let mut new_pods_so_far = pods_so_far.clone();
-        new_pods_so_far.push(Arc::new(remaining_pod));
+        new_pods_so_far.push(remaining_pod);
 
         let pod_combos = helper::<POD_COUNT>(new_pods_so_far, new_remaining_teams, conference);
         all_pod_combos.extend(pod_combos);
@@ -67,9 +68,37 @@ fn helper<const POD_COUNT: usize>(
     all_pod_combos
 }
 
+fn is_pod_valid(pod: &Pod) -> bool {
+    for team in pod {
+        let are_all_protected_pairs_in_pod = PROTECTED_PAIRS
+            .into_iter()
+            .filter(|(a, b)| a == team || b == team)
+            .map(|(a, b)| if a == *team { b } else { a })
+            .all(|opposing_team| pod.contains(&opposing_team));
+        if !are_all_protected_pairs_in_pod {
+            return false;
+        }
+    }
+    true
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn pod_is_valid() {
+        let pod = vec!["Indiana", "Purdue", "Illinois", "Northwestern"];
+
+        assert!(is_pod_valid(&pod));
+    }
+
+    #[test]
+    fn pod_is_not_valid() {
+        let pod = vec!["Indiana", "Michigan State", "Northwestern", "Ohio State"];
+
+        assert!(!is_pod_valid(&pod));
+    }
 
     #[test]
     fn contains_some_pod() {
@@ -92,6 +121,47 @@ mod test {
             // "Wisconsin",
         ];
 
-        let _ = get_all_pod_combinations::<3>(&conference);
+        let pod_combos = get_all_pod_combinations::<3>(&conference);
+        assert_eq!(pod_combos.len(), 7);
+        let expected: Vec<PodCombo<3>> = vec![
+            [
+                vec!["Indiana", "Northwestern", "Purdue", "Illinois"],
+                vec!["Ohio State", "Penn State", "Rutgers", "Maryland"],
+                vec!["Michigan State", "UCLA", "Iowa", "Michigan"],
+            ],
+            [
+                vec!["Indiana", "Northwestern", "Purdue", "Illinois"],
+                vec!["Ohio State", "Penn State", "UCLA", "Iowa"],
+                vec!["Michigan State", "Rutgers", "Maryland", "Michigan"],
+            ],
+            [
+                vec!["Indiana", "Northwestern", "Purdue", "Illinois"],
+                vec!["Ohio State", "Rutgers", "UCLA", "Maryland"],
+                vec!["Michigan State", "Penn State", "Iowa", "Michigan"],
+            ],
+            [
+                vec!["Indiana", "Northwestern", "Purdue", "Illinois"],
+                vec!["Ohio State", "Rutgers", "Iowa", "Maryland"],
+                vec!["Michigan State", "Penn State", "UCLA", "Michigan"],
+            ],
+            [
+                vec!["Indiana", "Northwestern", "Purdue", "Illinois"],
+                vec!["Penn State", "Rutgers", "UCLA", "Maryland"],
+                vec!["Michigan State", "Ohio State", "Iowa", "Michigan"],
+            ],
+            [
+                vec!["Indiana", "Northwestern", "Purdue", "Illinois"],
+                vec!["Penn State", "Rutgers", "Iowa", "Maryland"],
+                vec!["Michigan State", "Ohio State", "UCLA", "Michigan"],
+            ],
+            [
+                vec!["Indiana", "Northwestern", "Purdue", "Illinois"],
+                vec!["Rutgers", "UCLA", "Iowa", "Maryland"],
+                vec!["Michigan State", "Ohio State", "Penn State", "Michigan"],
+            ],
+        ];
+        for pod_combo in pod_combos {
+            assert!(expected.contains(&pod_combo));
+        }
     }
 }
